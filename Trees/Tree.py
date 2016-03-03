41bcb9cc39b0f47db8690741763b6204ad3cc908
Steve Beaty <drsjb80@gmail.com>
2016-03-03 15:40:58 -0700

    A fix to the recursive remove for two children.

--- Trees/Tree.py
+++ Trees/Tree.py
@@ -1,16 +1,23 @@
 from __future__ import print_function
 import unittest
 
+trace = False
+
 class binary_tree:
+  # create a unique empty value so None can be stored
+  __empty = {}
+
   def __init__ (self, value = None, parent = None):
-    self.__left = self.__right = self.__value = None
+    self.__left = self.__right = None
+    self.__value = self.__empty
     self.__parent = parent
 
-    try:
-      for i in value:
-        self.add(i)
-    except TypeError:
-      self.add(value)
+    if value:
+      try:
+        for i in value:
+          self.add(i)
+      except TypeError:
+        self.add(value)
 
   def __iter__(self):
     if self.__left:
@@ -23,11 +30,13 @@ class binary_tree:
       for node in self.__right:
         yield(node)
 
+  def empty(self): return self.__value == self.__empty
+
   def __str__(self): 
     return(','.join(str(node) for node in self))
 
   def add(self, value):
-    if self.__value == None:
+    if self.empty():
       self.__value = value
       return
 
@@ -55,9 +64,19 @@ class binary_tree:
     return(t)
 
   def __parent_replace_me_with(self, new):
+    if trace:
+      print("in:", self.__parent.__value, "replacing:", \
+        self.__value, "with:",)
+      if new:
+        print(new.__value,)
+      else:
+        print(new,)
+
     if self.__parent.__left == self:
+      if trace: print("on the left")
       self.__parent.__left = new
     elif self.__parent.__right == self:
+      if trace: print("on the right")
       self.__parent.__right = new
     else:
       assert(True)
@@ -65,19 +84,18 @@ class binary_tree:
   def __remove_both(self):
     # find the right-most of the left hand tree
     rightmost = self.__left.__find_rightmost()
+    if trace: print("rightmost:", rightmost.__value)
 
     # remove it
-    rightmost.__parent_replace_me_with(None)
-
-    # set its right and left to my right and left
-    rightmost.__left = self.__left
-    rightmost.__right = self.__right
+    self.remove(rightmost.__value)
 
     # tricky bit: if it's the root, don't change
     # the refenence but only the value
     if not self.__parent:
       self.__value = rightmost.__value
     else:
+      rightmost.__left = self.__left
+      rightmost.__right = self.__right
       self.__parent_replace_me_with(rightmost)
 
   def remove(self, value):
@@ -105,7 +123,7 @@ class test_binary_tree (unittest.TestCase):
   '''
 
   def test_empty(self):
-    self.assertEquals(str(binary_tree()), 'None')
+    self.assertTrue(binary_tree().empty())
 
   def test_add(self):
     bt = binary_tree()
@@ -139,6 +157,20 @@ class test_binary_tree (unittest.TestCase):
     bt = binary_tree([20, 10, 15, 30, 25, 35])
     bt.remove(30)
     self.assertEquals(str(bt), '10,15,20,25,35')
+
+  '''
+   20
+  /  \
+10    30
+     /  \
+    25  35
+   /
+  24
+  '''
+  def test_remove_both_again(self):
+    bt = binary_tree([20, 10, 30, 25, 24, 35])
+    bt.remove(30)
+    self.assertEquals(str(bt), '10,20,24,25,35')
 
   def test_remove_root(self):
     bt = binary_tree([20, 10, 30, 25, 35])
