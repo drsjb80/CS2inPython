81323d3c326ac221aca12b38a582f720377dd625
Steve Beaty <drsjb80@gmail.com>
2016-03-17 20:29:06 -0600

    some cleanup and fix for BFS

--- Graphs/graph.py
+++ Graphs/graph.py
@@ -4,28 +4,23 @@ import sys
 
 class graph:
   def __init__(self, nodes=None, edges=None):
-    self.__nodes = []
-    self.__edges = []
+    self.__nodes = set()
+    self.__edges = set()
 
     if nodes:
       for node in nodes:
-        self.add_node(node)
+        self.__nodes.add(node)
 
     if edges:
       for edge in edges:
-        self.add_edge(edge)
+        self.__edges.add(edge)
 
   # return immutable tuples
   def nodes(self): return(tuple(self.__nodes))
   def edges(self): return(tuple(self.__edges))
 
-  def add_node(self, node):
-    if not node in self.__nodes:
-      self.__nodes.append(node)
-
-  def add_edge(self, edge):
-    if not edge in self.__edges:
-      self.__edges.append(edge)
+  def add_node(self, node): self.__nodes.add(node) 
+  def add_edge(self, edge): self.__edges.add(edge)
   
   def adjacent(self, node):
     result = ()
@@ -37,9 +32,8 @@ class graph:
 
   """Find a path through a graph"""
   def DFS(self, start, target):
-    stack = []
+    stack = [start]
     visited = []
-    stack.append(start)
 
     while stack:
       current = stack.pop()
@@ -64,24 +58,23 @@ class graph:
 
     return None
 
-
-  """Find all paths"""
   def BFS(self, start, target):
-    stack = []
+    queue = [start]
     visited = []
-    stack.append(start)
 
-    while stack:
-      current = stack.pop(0)
+    while queue:
+      current = queue.pop(0)
+
       if current in visited: continue
+
       visited.append(current)
 
       if current == target: return visited
 
       for v in self.adjacent(current):
-        if v not in visited:
-          stack.append(v)
-
+        if v not in visited and v not in queue:
+          queue.append(v)
+      
     return None
 
   def __str__(self):
@@ -107,41 +100,42 @@ class test_graph(unittest.TestCase):
     self.assertEqual(len(g.nodes()), 2)
     self.assertEqual(len(g.edges()), 1)
     self.assertEqual(str(g), 'nodes:(1, 2), edges:((1, 2),)')
-    g.add_edge((2, 1))
-    self.assertEqual(len(g.edges()), 2)
-    self.assertEqual(str(g), 'nodes:(1, 2), edges:((1, 2, 3), (2, 1))')
   def test_init(self):
-    g = graph([1, 2], [(1, 2), (2, 1)])
-    self.assertEqual(str(g), 'nodes:(1, 2), edges:((1, 2), (2, 1))')
+    g = graph([1, 2], [(1, 2)])
+    self.assertEqual(str(g), 'nodes:(1, 2), edges:((1, 2),)')
   def test_adjacent(self):
     g = graph([1, 2], [(1, 2), (2, 1)])
     self.assertEqual(g.adjacent(1), (2,))
+  def test_more_adjacent(self):
+    g = graph([1, 2, 3, 4, 5, 6], [(1,2), (1,3), (2,3), (2,4), (2,5), \
+      (3,5), (4,5), (4,6), (5,6)])
+    self.assertEqual(g.adjacent(2), (1, 3, 5, 4,))
   def test_strings(self):
     g = graph(['Denver', 'Boulder'], [('Denver', 'Boulder')])
-    self.assertEqual(str(g), "nodes:('Denver', 'Boulder'), \
+    self.assertEqual(str(g), "nodes:('Boulder', 'Denver'), \
 edges:(('Denver', 'Boulder'),)")
   def test_DFS(self):
-    """
+    '''
       2---4
      /|\  |\
     1 | \ | 6
      \|  \|/
       3---5
-    """
+    '''
     g = graph([1, 2, 3, 4, 5, 6], [(1,2), (1,3), (2,3), (2,4), (2,5), \
       (3,5), (4,5), (4,6), (5,6)])
-    self.assertEquals(g.DFS(1, 6), [1, 3, 5, 6])
-    self.assertEquals(g.DFS(3, 4), [3, 5, 6, 4])
+    self.assertEquals(g.DFS(1, 6), [1, 3, 5, 2, 4, 6])
+    self.assertEquals(g.DFS(3, 4), [3, 5, 2, 4])
     self.assertEquals(g.DFS(1, 7), None)
-  def test_rBFS(self):
+  def test_rDFS(self):
     g = graph([1, 2, 3, 4, 5, 6], [(1,2), (1,3), (2,3), (2,4), (2,5), \
       (3,5), (4,5), (4,6), (5,6)])
     self.assertEquals(g.rDFS(1, 6, []), [1, 2, 3, 5, 4, 6])
-    self.assertEquals(g.rDFS(3, 4, []), [3, 1, 2, 4])
+    self.assertEquals(g.rDFS(3, 4, []), [3, 1, 2, 5, 4])
     self.assertEquals(g.rDFS(1, 7, []), None)
   def test_BFS(self):
     g = graph([1, 2, 3, 4, 5, 6], [(1,2), (1,3), (2,3), (2,4), (2,5), \
       (3,5), (4,5), (4,6), (5,6)])
-    self.assertEquals(g.BFS(1, 6), [1, 2, 3, 4, 5, 6])
-    self.assertEquals(g.BFS(3, 4), [3, 1, 2, 5, 4])
+    self.assertEquals(g.BFS(1, 6), [1, 2, 3, 5, 4, 6])
+    self.assertEquals(g.BFS(5, 1), [5, 4, 6, 2, 3, 1])
     self.assertEquals(g.BFS(1, 7), None)
