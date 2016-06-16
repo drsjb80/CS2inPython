e9055c3b6c4faa002c9cc9df2dc49b19a6612ce0
Steve Beaty <drsjb80@gmail.com>
2016-06-16 15:36:57 -0600

    a better explaination

--- Recursion/reverse.py
+++ Recursion/reverse.py
@@ -2,9 +2,8 @@ def reverse(a):
     result = type (a)()
     if a == result: return (result)
 
-    result += reverse(a[1:]) + a[:1]
-
-    return (result)
+    # the answer is the rest reversed followed by the first
+    return reverse(a[1:]) + a[:1]
 
 print (reverse("hello"))
 print (reverse([1, 2, 3, 4, 5]))
