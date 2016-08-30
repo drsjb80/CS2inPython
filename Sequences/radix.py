342252652e47fd7505afcb54a92a6b42ed191933
Steve Beaty <drsjb80@gmail.com>
2016-08-30 14:46:41 -0600

    after some pylint cleanup

--- Sequences/radix.py
+++ Sequences/radix.py
@@ -2,67 +2,69 @@ from __future__ import print_function
 import unittest
 import sys
 
-trace = False
+TRACE = False
 
-''' Extend str and overwrite one method so that
-requests for characters beyond the last one
-return a space'''
-class mystr(str):
+class Mystr(str):
+    ''' Extend str and overwrite one method so that
+    requests for characters beyond the last one
+    return a space'''
     def __getitem__(self, num):
         if num > len(self)-1: return " "
-        return super(mystr, self).__getitem__(num)
+        return super(Mystr, self).__getitem__(num)
 
-class test_mystr(unittest.TestCase):
+class TestMystr(unittest.TestCase):
+    def setUp(self):
+        self.string = Mystr("A")
     def test_empty(self):
         self.assertEquals("", "")
     def test_simple(self):
-        a = mystr("A")
-        self.assertEquals(a[0], "A")
+        self.assertEquals(self.string[0], "A")
     def test_missing(self):
-        a = mystr("A")
-        self.assertEquals(a[10], " ")
+        self.assertEquals(self.string[10], " ")
     def test_longer(self):
-        a = mystr("A")
-        self.assertEquals(a[10], " ")
+        self.assertEquals(self.string[10], " ")
 
-def radix_sort(list, longest):
+def radix_sort(array, longest):
     for position in range(longest-1, -1, -1):
         buckets = [[] for _ in range(128)]
 
-        for word in list:
+        for word in array:
             where = ord(word[position])
             buckets[where].append(word)
 
-        list = []
+        array = []
         for bucket in buckets:
-            list.extend(bucket)
+            array.extend(bucket)
 
-    return list
+    return array
 
-class test_radix_sort(unittest.TestCase):
+class TestRadixSort(unittest.TestCase):
     def test_simple(self):
-        self.assertEquals(radix_sort([mystr("A")], 1), ["A"])
+        self.assertEquals(radix_sort([Mystr("A")], 1), ["A"])
     def test_two(self):
-        self.assertEquals(radix_sort([mystr("A"), mystr("A")], 1), ["A", "A"])
+        self.assertEquals(radix_sort([Mystr("A"), Mystr("A")], 1), ["A", "A"])
     def test_different(self):
-        self.assertEquals(radix_sort([mystr("A"), mystr("B")], 1), ["A", "B"])
+        self.assertEquals(radix_sort([Mystr("A"), Mystr("B")], 1), ["A", "B"])
     def test_five(self):
-        self.assertEquals(radix_sort([mystr("AA"), mystr("AB")], 2), \
+        self.assertEquals(radix_sort([Mystr("AA"), Mystr("AB")], 2), \
             ["AA", "AB"])
     def test_six(self):
-        self.assertEquals(radix_sort([mystr("AA"), mystr("BA")], 2), \
+        self.assertEquals(radix_sort([Mystr("AA"), Mystr("BA")], 2), \
             ["AA", "BA"])
     def test_seven(self):
-        self.assertEquals(radix_sort([mystr("AA"), mystr("A")], 2), \
+        self.assertEquals(radix_sort([Mystr("AA"), Mystr("A")], 2), \
             ["A", "AA"])
 
-if "__main__" == __name__:
+
+def run():
     words = []
     longest = 0
-    for line in sys.stdin:
-        l = line.strip()
-        words.append(mystr(l))
-        if len(l) > longest: longest = len(l)
+    for lines in sys.stdin:
+        line = lines.strip()
+        words.append(Mystr(line))
+        if len(line) > longest: longest = len(line)
 
     print(words)
     print(radix_sort(words, longest))
+
+if "__main__" == __name__: run()
