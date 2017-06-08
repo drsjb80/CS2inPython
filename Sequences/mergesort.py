c2b7c0af7b5655c0ceb513f351a8894a2982b226
Steven Beaty <beatys@ms16142.local>
2017-06-08 10:26:22 -0600

    More cleanup and have main sort the programs standard input.

--- Sequences/mergesort.py
+++ Sequences/mergesort.py
@@ -1,9 +1,10 @@
 from __future__ import print_function
 import unittest
+import random
 
 TRACE = False
 
-def merge(one, two):
+def _merge(one, two):
     '''Merge two already sorted lists and return a single list.'''
     if not one: return two
     if not two: return one
@@ -24,7 +25,29 @@ def merge(one, two):
 
     return result
 
-def sort(array):
+class TestMerge(unittest.TestCase):
+    def test_both_none(self):
+        self.assertEquals(_merge(None, None), None)
+    def test_one_none(self):
+        self.assertEquals(_merge(None, [1, 2, 3]), [1, 2, 3])
+    def test_two_none(self):
+        self.assertEquals(_merge([1, 2, 3], None), [1, 2, 3])
+    def test_one_zero(self):
+        self.assertEquals(_merge([], [1, 2, 3]), [1, 2, 3])
+    def test_two_zero(self):
+        self.assertEquals(_merge([1, 2, 3], []), [1, 2, 3])
+    def test_one_one(self):
+        self.assertEquals(_merge([1], []), [1])
+    def test_two_one(self):
+        self.assertEquals(_merge([], [1]), [1])
+    def test_one_another_one(self):
+        self.assertEquals(_merge([1], [1]), [1, 1])
+    def test_one_two(self):
+        self.assertEquals(_merge([1], [1, 2]), [1, 1, 2])
+    def test_float(self):
+        self.assertEquals(_merge([1.0], [2, 3]), [1.0, 2, 3])
+
+def merge_sort(array):
     '''Do the actual sorting:
         Start with combining every other sublist as each individual element
         is, by definition, sorted. That gives us sorted lists of length
@@ -38,42 +61,36 @@ def sort(array):
     if TRACE: print("result:", result)
 
     while len(result) != 1:
-        result.append(merge(result.pop(0), result.pop(0)))
+        result.append(_merge(result.pop(0), result.pop(0)))
 
     return result[0]
 
-class TestMerge(unittest.TestCase):
-    def test_both_none(self):
-        self.assertEquals(merge(None, None), None)
-    def test_one_none(self):
-        self.assertEquals(merge(None, [1, 2, 3]), [1, 2, 3])
-    def test_two_none(self):
-        self.assertEquals(merge([1, 2, 3], None), [1, 2, 3])
-    def test_one_zero(self):
-        self.assertEquals(merge([], [1, 2, 3]), [1, 2, 3])
-    def test_two_zero(self):
-        self.assertEquals(merge([1, 2, 3], []), [1, 2, 3])
-    def test_one_one(self):
-        self.assertEquals(merge([1], []), [1])
-    def test_two_one(self):
-        self.assertEquals(merge([], [1]), [1])
-    def test_one_another_one(self):
-        self.assertEquals(merge([1], [1]), [1, 1])
-    def test_one_two(self):
-        self.assertEquals(merge([1], [1, 2]), [1, 1, 2])
-    def test_float(self):
-        self.assertEquals(merge([1.0], [2, 3]), [1.0, 2, 3])
-
 class TestSort(unittest.TestCase):
     def test_none(self):
-        self.assertEquals(sort([]), [])
+        self.assertEquals(merge_sort([]), [])
     def test_one(self):
-        self.assertEquals(sort([1]), [1])
+        self.assertEquals(merge_sort([1]), [1])
     def test_two(self):
-        self.assertEquals(sort([2, 1]), [1, 2])
+        self.assertEquals(merge_sort([2, 1]), [1, 2])
     def test_four(self):
-        self.assertEquals(sort([4, 3, 2, 1]), [1, 2, 3, 4])
+        self.assertEquals(merge_sort([4, 3, 2, 1]), [1, 2, 3, 4])
+    def test_99(self):
+        a = range(99)
+        random.shuffle(a)
+        b = a[:]
+        b.sort()
+        self.assertEquals(merge_sort(a), b)
+    def test_100(self):
+        a = range(100)
+        random.shuffle(a)
+        b = a[:]
+        b.sort()
+        self.assertEquals(merge_sort(a), b)
 
 if "__main__" == __name__:
-    print(sort([4, 3, 2, 1]))
-    print(sort([5, 4, 3, 2, 1]))
+    lines = []
+    for line in sys.stdin:
+        lines.append(line.strip())
+
+    print(merge_sort(lines))
+
